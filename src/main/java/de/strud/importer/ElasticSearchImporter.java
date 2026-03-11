f6f974520cae61c4cadb4aef853a9670c5b9ad30
Stefan Rudnitzki <1756316+stefzki@users.noreply.github.com>
2026-03-11 14:29:31 +0100

    upgraded to Elasticsearch 9

--- src/main/java/de/strud/importer/ElasticSearchImporter.java
+++ src/main/java/de/strud/importer/ElasticSearchImporter.java
@@ -2,17 +2,17 @@ package de.strud.importer;
 
 import java.io.IOException;
 
-import org.apache.http.HttpHost;
+import org.apache.hc.core5.http.HttpHost;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.elasticsearch.client.RestClient;
 
 import co.elastic.clients.elasticsearch.ElasticsearchClient;
 import co.elastic.clients.elasticsearch.core.IndexRequest;
 import co.elastic.clients.elasticsearch.core.IndexResponse;
 import co.elastic.clients.json.jackson.JacksonJsonpMapper;
 import co.elastic.clients.transport.ElasticsearchTransport;
-import co.elastic.clients.transport.rest_client.RestClientTransport;
+import co.elastic.clients.transport.rest5_client.Rest5ClientTransport;
+import co.elastic.clients.transport.rest5_client.low_level.Rest5Client;
 import de.strud.data.Document;
 
 /**
@@ -21,28 +21,31 @@ import de.strud.data.Document;
 public class ElasticSearchImporter implements DBImporter {
 
     private static final Logger LOG = LogManager.getLogger(ElasticSearchImporter.class);
+
     private final ElasticsearchTransport transport;
 
+    private final ElasticsearchClient client;
+
     public ElasticSearchImporter(String host, int port) {
-        RestClient httpClient = RestClient.builder(
+        Rest5Client httpClient = Rest5Client.builder(
                 new HttpHost(host, port)
             ).build();
 
-        transport = new RestClientTransport(httpClient, new JacksonJsonpMapper());
+        transport = new Rest5ClientTransport(httpClient, new JacksonJsonpMapper());
+        client = new ElasticsearchClient(transport);
     }
 
 
     @Override
     public boolean importDocument(Document document) {
         try {
-            ElasticsearchClient esClient = new ElasticsearchClient(transport);
             IndexRequest<Document> request = IndexRequest.of(i -> i
                 .index("wikipedia")
                 .id(document.getUrl())
                 .document(document)
             );
 
-            IndexResponse response = esClient.index(request);
+            IndexResponse response = client.index(request);
         } catch (IOException e) {
             LOG.error("Cannot index document.", e);
             return false;
